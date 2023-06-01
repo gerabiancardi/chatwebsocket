990c8ecd276d6b4ffbf6b32d02716d51795033b1
Gerardo Biancardi <gerabiancardi@gmail.com>
2023-06-01 17:16:56 -0300

    Tercera Actualziacion

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,4 +1,4 @@
-const socket= io();
+const socket = io();
 /* 
 Swal.fire({
     title:"Saludos",
@@ -10,48 +10,46 @@ Swal.fire({
 let user;
 const chatbox = document.getElementById("chatBox");
 
-Swal.fire(
-    {
-        title:"Indentifiquese",
-        input: "text",
-        text: "Ingrese su nombre de usuario para ingresar al chat",
-        inputValidator: (value)=>{
-            return !value && "Necesitas un nombre de usuario para ingresar al chat"
-        },
-        allowOutsideClick:false, 
-        allowEscapeKey:false
-    }
-).then (result=>{
-    user= result.value;
-    socket.emit("authenticated", user);
+Swal.fire({
+  title: "Indentifiquese",
+  input: "text",
+  text: "Ingrese su nombre de usuario para ingresar al chat",
+  inputValidator: (value) => {
+    return !value && "Necesitas un nombre de usuario para ingresar al chat";
+  },
+  allowOutsideClick: false,
+  allowEscapeKey: false,
+}).then((result) => {
+  user = result.value;
+  socket.emit("authenticated", user);
 });
 
-chatbox.addEventListener("keyup", evt=>{
-    if (evt.key === "Enter"){
-        if(chatbox.value.trim().lenght >0){
-            socket.emit("message", {user, message: chatbox.value});
-             chatbox.value = "";
-        }
+chatbox.addEventListener("keyup", (evt) => {
+  if (evt.key === "Enter") {
+    console.log("Apreto Enter")
+    if (chatbox.value.trim().lenght > 0) {
+      socket.emit("message", { user, message: chatbox.value });
+      chatbox.value = "";
     }
-})
-
-socket.on("messageLogs", data=>{
-    let log= document.getElementById("messageLogs");
-    let messages= "";
-    data.forEach(message=>{
-        messages += `${message.user} dice: ${message.message}<br/>`
-    });
-    log.innerHTML=messages;
+  }
 });
 
-socket.on("NewUserConnected", data=>{
-    Swal.fire({
-        toast: true,
-        position: "top-end",
-        showConfirmButton: false,
-        timmer: 3000,
-        title: `${data} se ha unido al chat`,
-        icon: "success"
-    })
-})
+socket.on("messageLogs", (data) => {
+  let log = document.getElementById("messageLogs");
+  let messages = "";
+  data.forEach((message) => {
+    messages += `${message.user} dice: ${message.message}<br/>`;
+  });
+  log.innerHTML = messages;
+});
 
+socket.on("NewUserConnected", (data) => {
+  Swal.fire({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timmer: 3000,
+    title: `${data} se ha unido al chat`,
+    icon: "success",
+  });
+});
